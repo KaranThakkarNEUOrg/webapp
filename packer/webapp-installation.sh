6694fa96a0e85c31c04c79fb4c90683525cff33e
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 17:57:11 -0500

    workflow changes

--- packer/webapp-installation.sh
+++ packer/webapp-installation.sh
@@ -5,10 +5,3 @@ sudo dnf install unzip -y
 sudo unzip /tmp/webapp.zip -d /opt/webapp/
 cd /opt/webapp/
 sudo npm install
-sudo touch .env
-echo "MYSQL_HOSTNAME=localhost" | sudo tee -a .env
-echo "MYSQL_PASSWORD=root" | sudo tee -a .env
-echo "MYSQL_DATABASENAME=cloud_assignment02" | sudo tee -a .env
-echo "MYSQL_USERNAME=root" | sudo tee -a .env
-echo "SALT_ROUNDS=10" | sudo tee -a .env
-echo "PORT=8888" | sudo tee -a .env
