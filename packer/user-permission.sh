b1f67a981739ab6f7328ac19dcf61a0c76b71258
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 17:08:44 -0500

    password change

--- packer/user-permission.sh
+++ packer/user-permission.sh
@@ -4,9 +4,9 @@ sudo groupadd csye6225
 sudo useradd -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown -R csye6225:csye6225 /opt/webapp/
 sudo chown -R csye6225:csye6225 /tmp/webapp.service
-sudo chmod 700 /opt/webapp/
-sudo chmod 700 /tmp/webapp.service
 sudo cp /tmp/webapp.service /etc/systemd/system/
+sudo chmod 700 /opt/webapp/
+sudo chmod 700 /etc/systemd/system/
 echo "csye6225 ALL=(ALL:ALL) NOPASSWD: /bin/systemctl" | sudo EDITOR='tee -a' visudo
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
