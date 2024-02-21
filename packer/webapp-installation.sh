04e2fbcec97e88b0d5b6d2cc5af223c13361ea31
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 13:41:21 -0500

    permission change

--- packer/webapp-installation.sh
+++ packer/webapp-installation.sh
@@ -6,9 +6,9 @@ sudo unzip /tmp/webapp.zip -d /home/packer/webapp/
 cd /home/packer/webapp/
 sudo npm install
 sudo touch .env
-sudo echo "MYSQL_HOSTNAME=localhost" >>.env
-sudo echo "MYSQL_PASSWORD=Karan@mysql001" >>.env
-sudo echo "MYSQL_DATABASENAME=cloud_assignment02" >>.env
-sudo echo "MYSQL_USERNAME=root" >>.env
-sudo echo "SALT_ROUNDS=10" >>.env
-sudo echo "PORT=8888" >>.env
+echo "MYSQL_HOSTNAME=localhost" | sudo tee -a .env
+echo "MYSQL_PASSWORD=Karan@mysql001" | sudo tee -a .env
+echo "MYSQL_DATABASENAME=cloud_assignment02" | sudo tee -a .env
+echo "MYSQL_USERNAME=root" | sudo tee -a .env
+echo "SALT_ROUNDS=10" | sudo tee -a .env
+echo "PORT=8888" | sudo tee -a .env
