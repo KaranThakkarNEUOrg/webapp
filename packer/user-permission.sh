19c2f28831cafb603f53cf5197e73236d66618d5
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 14:28:18 -0500

    path change

--- packer/user-permission.sh
+++ packer/user-permission.sh
@@ -2,6 +2,6 @@
 
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 sudo chown -R csye6225:csye6225 /home/packer/
-sudo cp /home/packer/webapp/webapp.service /etc/systemd/system/
+sudo cp /home/packer/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
