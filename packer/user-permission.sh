af18305dc382658306577602acd15a1112ba009c
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 14:53:58 -0500

    usergroup add

--- packer/user-permission.sh
+++ packer/user-permission.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
+sudo groupadd csye6225
+sudo useradd -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown -R csye6225:csye6225 /home/packer/
 sudo cp /home/packer/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
