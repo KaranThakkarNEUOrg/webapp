c101e008f1b385f94fa033cf7e2d7f3b0d771d88
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 04:32:06 -0500

    chown bug fix

--- packer/permissions.sh
+++ packer/permissions.sh
@@ -2,5 +2,5 @@
 
 sudo chmod +x /tmp/mysql.sh
 sudo chmod +x /tmp/nodejs.sh
-sudo chown -R packer:packer packer/
-sudo chmod -R 755 packer/
+sudo chown -R packer:packer /home/packer/
+sudo chmod -R 755 /home/packer/
