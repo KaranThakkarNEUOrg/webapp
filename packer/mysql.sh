a33c16c84adb550abcf69c1a607b268cd8617351
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 03:18:00 -0500

    yml changes

--- packer/mysql.sh
+++ packer/mysql.sh
@@ -3,5 +3,5 @@
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
 sudo systemctl enable mysqld
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '\$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | sudo mysql
-echo "CREATE DATABASE cloud_assignment02;" | sudo mysql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}'; FLUSH PRIVILEGES;" | sudo mysql
+echo "CREATE DATABASE cloud_assignment02;" | sudo mysql -u root -p'${MYSQL_PASSWORD}'
