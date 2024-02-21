d35451d69027b202163add0ef7954c2807ea5cb1
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 17:57:11 -0500

    workflow changes

--- packer/user-permission.sh
+++ packer/user-permission.sh
@@ -3,10 +3,10 @@
 sudo groupadd csye6225
 sudo useradd -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown -R csye6225:csye6225 /opt/webapp/
-sudo chown -R csye6225:csye6225 /tmp/webapp.service
-sudo cp /tmp/webapp.service /etc/systemd/system/
+sudo chown -R csye6225:csye6225 /etc/systemd/system/webapp.service
+# sudo cp /tmp/webapp.service /etc/systemd/system/
 sudo chmod 700 /opt/webapp/
-sudo chmod 700 /etc/systemd/system/
+sudo chmod 700 /etc/systemd/system/webapp.service
 echo "csye6225 ALL=(ALL:ALL) NOPASSWD: /bin/systemctl" | sudo EDITOR='tee -a' visudo
 sudo systemctl daemon-reload
 sudo systemctl start webapp
---
+++ packer/webapp-service.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo sh -c 'cat << EOF > /etc/systemd/system/webapp.service
+[Unit]
+Description=Node.js Application
+After=network.target
+
+[Service]
+Type=simple
+User=csye6225
+Group=csye6225
+Environment=MYSQL_HOSTNAME='$MYSQL_HOSTNAME'
+Environment=MYSQL_PASSWORD='$MYSQL_PASSWORD'
+Environment=MYSQL_DATABASENAME='$MYSQL_DATABASENAME'
+Environment=MYSQL_USERNAME='$MYSQL_USERNAME'
+Environment=SALT_ROUNDS='$SALT_ROUNDS'
+Environment=PORT='$PORT'
+WorkingDirectory=/opt/webapp/
+ExecStart=/usr/bin/node /opt/webapp/index.js
+Restart=on-failure
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
