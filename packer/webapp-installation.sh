2425295f1e01023c432dbc27004c4570e68e4824
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 12:21:48 -0500

    root permissions

--- packer/webapp-installation.sh
+++ packer/webapp-installation.sh
@@ -2,10 +2,10 @@
 
 cd /
 sudo dnf install unzip -y
-unzip cd /tmp/packer/webapp.zip -d cd /home/packer/webapp/
+sudo unzip cd /tmp/packer/webapp.zip -d cd /home/packer/webapp/
 cd home/packer/webapp/
-npm install
-touch .env
+sudo npm install
+sudo touch .env
 echo "MYSQL_HOSTNAME=localhost" >>.env
 echo "MYSQL_PASSWORD=Karan@mysql001" >>.env
 echo "MYSQL_DATABASENAME=cloud_assignment02" >>.env
