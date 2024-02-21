b1c542bd91aaa8d52f43cb6a0b51210a39a5ab9f
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 17:09:17 -0500

    password change

--- packer/webapp-installation.sh
+++ packer/webapp-installation.sh
@@ -7,7 +7,7 @@ cd /opt/webapp/
 sudo npm install
 sudo touch .env
 echo "MYSQL_HOSTNAME=localhost" | sudo tee -a .env
-echo "MYSQL_PASSWORD=Karan@mysql001" | sudo tee -a .env
+echo "MYSQL_PASSWORD=root" | sudo tee -a .env
 echo "MYSQL_DATABASENAME=cloud_assignment02" | sudo tee -a .env
 echo "MYSQL_USERNAME=root" | sudo tee -a .env
 echo "SALT_ROUNDS=10" | sudo tee -a .env
