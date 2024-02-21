705ad3ebca63b541b920d711b7f50d31fcd3177b
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 12:53:02 -0500

    installation script added

--- packer/webapp-installation.sh
+++ packer/webapp-installation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# cd /
-# sudo dnf install unzip -y
-# sudo unzip /tmp/packer/webapp.zip -d /home/packer/webapp/
-# cd /home/packer/webapp/
-# sudo npm install
-# sudo touch .env
-# echo "MYSQL_HOSTNAME=localhost" >>.env
-# echo "MYSQL_PASSWORD=Karan@mysql001" >>.env
-# echo "MYSQL_DATABASENAME=cloud_assignment02" >>.env
-# echo "MYSQL_USERNAME=root" >>.env
-# echo "SALT_ROUNDS=10" >>.env
-# echo "PORT=8888" >>.env
+cd /
+sudo dnf install unzip -y
+sudo unzip /tmp/webapp.zip -d /home/packer/webapp/
+cd /home/packer/webapp/
+sudo npm install
+sudo touch .env
+echo "MYSQL_HOSTNAME=localhost" >>.env
+echo "MYSQL_PASSWORD=Karan@mysql001" >>.env
+echo "MYSQL_DATABASENAME=cloud_assignment02" >>.env
+echo "MYSQL_USERNAME=root" >>.env
+echo "SALT_ROUNDS=10" >>.env
+echo "PORT=8888" >>.env
