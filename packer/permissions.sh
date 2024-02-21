08b0acd96c358454c66a0c19a5db536a2693e673
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-21 14:39:50 -0500

    added files to provisioner

--- packer/permissions.sh
+++ packer/permissions.sh
@@ -3,5 +3,6 @@
 sudo chmod +x /tmp/mysql.sh
 sudo chmod +x /tmp/nodejs.sh
 sudo chmod +x /tmp/webapp-installation.sh
+sudo chmod +x /tmp/user-permission.sh
 # sudo chown -R packer:packer /home/packer/
 # sudo chmod -R 755 /home/packer/
