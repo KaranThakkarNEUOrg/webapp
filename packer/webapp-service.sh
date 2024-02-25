905c131ba4fd512bbed001904e2ed79304b624e2
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-25 16:29:36 -0500

    packer image changes

--- packer/webapp-service.sh
+++ packer/webapp-service.sh
@@ -11,9 +11,12 @@ Type=simple
 User=csye6225
 Group=csye6225
 WorkingDirectory=/opt/webapp/
+ExecStartPre=/bin/bash -c 'while [ ! -f /opt/finish ]; do sleep 1; done'
 ExecStart=/usr/bin/node /opt/webapp/index.js
 Restart=on-failure
 RestartSec=3
+StandardOutput=append:/var/log/webapp_output.log
+StandardError=append:/var/log/webapp_error.log
 
 [Install]
 WantedBy=multi-user.target
