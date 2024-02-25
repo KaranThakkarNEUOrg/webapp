ccaf15a916ac80ddf2d01d46ecc89632fc285f15
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-25 02:32:21 -0500

    removed mysql installation

--- packer/webapp-service.sh
+++ packer/webapp-service.sh
@@ -3,18 +3,13 @@
 sudo sh -c 'cat << EOF > /etc/systemd/system/webapp.service
 [Unit]
 Description=Node.js Application
-After=network.target
+After=network.target network-online.target
+Wants=network-online.target
 
 [Service]
 Type=simple
 User=csye6225
 Group=csye6225
-Environment=MYSQL_HOSTNAME='$MYSQL_HOSTNAME'
-Environment=MYSQL_PASSWORD='$MYSQL_PASSWORD'
-Environment=MYSQL_DATABASENAME='$MYSQL_DATABASENAME'
-Environment=MYSQL_USERNAME='$MYSQL_USERNAME'
-Environment=SALT_ROUNDS='$SALT_ROUNDS'
-Environment=PORT='$PORT'
 WorkingDirectory=/opt/webapp/
 ExecStart=/usr/bin/node /opt/webapp/index.js
 Restart=on-failure
