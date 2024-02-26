d5489dbda5addf1ed30e6aaf894a8cc1a3595826
Karan Thakkar <144720103+KaranThakkarNEU@users.noreply.github.com>
2024-02-26 22:55:41 +0530

    Update webapp-service.sh

--- packer/webapp-service.sh
+++ packer/webapp-service.sh
@@ -6,16 +6,19 @@ Description=Node.js Application
 After=network.target
 
 [Service]
-EnvironmentFile=/opt/webapp/.env
 Type=simple
 User=csye6225
 Group=csye6225
+Environment=MYSQL_HOSTNAME='$MYSQL_HOSTNAME'
+Environment=MYSQL_PASSWORD='$MYSQL_PASSWORD'
+Environment=MYSQL_DATABASENAME='$MYSQL_DATABASENAME'
+Environment=MYSQL_USERNAME='$MYSQL_USERNAME'
+Environment=SALT_ROUNDS='$SALT_ROUNDS'
+Environment=PORT='$PORT'
 WorkingDirectory=/opt/webapp/
 ExecStart=/usr/bin/node /opt/webapp/index.js
 Restart=on-failure
 RestartSec=3
-StandardOutput=append:/var/log/webapp_output.log
-StandardError=append:/var/log/webapp_error.log
 
 [Install]
 WantedBy=multi-user.target
