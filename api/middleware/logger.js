797578efbeff1641f68bf4b42ac014fd3c538886
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-03-16 18:50:18 -0400

    file path update

--- api/middleware/logger.js
+++ api/middleware/logger.js
@@ -7,7 +7,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "app.log" }),
+    new winston.transports.File({ filename: "/var/log/webapp/app.log" }),
   ],
 });
 
