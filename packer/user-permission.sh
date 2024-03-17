5ae10cf87be57acbe5d3c4baeeecb66516ff7ca7
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-03-17 14:17:01 -0400

    permission update

--- packer/user-permission.sh
+++ packer/user-permission.sh
@@ -6,6 +6,7 @@ sudo chown -R csye6225:csye6225 /opt/webapp/ /var/log/webapp/
 sudo chown -R csye6225:csye6225 /etc/systemd/system/webapp.service
 # sudo cp /tmp/webapp.service /etc/systemd/system/
 sudo chmod 700 /opt/webapp/ /var/log/webapp/
+sudo chmod +w /var/log/webapp/
 sudo chmod 700 /etc/systemd/system/webapp.service
 echo "csye6225 ALL=(ALL:ALL) NOPASSWD: /bin/systemctl" | sudo EDITOR='tee -a' visudo
 sudo systemctl daemon-reload
