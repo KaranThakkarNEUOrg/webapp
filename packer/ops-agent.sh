14ef74d3694ddb0233af1825e63014aff3cbd3ca
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-03-17 13:55:01 -0400

    config file update

--- packer/ops-agent.sh
+++ packer/ops-agent.sh
@@ -2,3 +2,24 @@
 
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+content="logging:
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/webapp/app.log
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor]
+"
+echo "$content" | sudo tee -a /etc/google-cloud-ops-agent/config.yaml
+sudo systemctl restart google-cloud-ops-agent
