3d144705e1abaf19200b45f809a45818192cffe1
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-03-26 21:47:50 -0400

    added authentication middlerware

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,6 +7,7 @@ const {
   verifyUser,
 } = require("../controllers/user");
 const authMiddleware = require("../middleware/auth");
+const verifiedUser = require("../middleware/verifiedUser");
 const checkContentLength = require("../middleware/contentLength");
 const logger = require("../middleware/logger");
 
@@ -80,7 +81,7 @@ const logger = require("../middleware/logger");
  *               items:
  *                 $ref: '#/components/schemas/User'
  */
-router.get("/self", checkContentLength, authMiddleware, getUser);
+router.get("/self", checkContentLength, authMiddleware, verifiedUser, getUser);
 router.get("/verifyUser", verifyUser);
 
 /**
@@ -122,7 +123,7 @@ router.post("/", createUser);
  *                 $ref: '#/components/schemas/User'
  *                 x-properties: first_name, last_name, password, username
  */
-router.put("/self", authMiddleware, updateUser);
+router.put("/self", authMiddleware, verifiedUser, updateUser);
 
 router.all("/", (req, res) => {
   logger.error(`Invalid endpoint or method`, {
